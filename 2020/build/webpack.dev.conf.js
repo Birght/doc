9d40a749cf0dbbd11493b77dbba96c217d8c6def
shi <shi_19890905@163.com>
2020-07-24 15:04:44 +0800

    shi 依赖追加

--- 2020/build/webpack.dev.conf.js
+++ 2020/build/webpack.dev.conf.js
@@ -8,6 +8,8 @@ const baseWebpackConfig = require('./webpack.base.conf')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
+// 新增打包分析
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 const portfinder = require('portfinder')
 
 const HOST = process.env.HOST
@@ -64,7 +66,9 @@ const devWebpackConfig = merge(baseWebpackConfig, {
         to: config.dev.assetsSubDirectory,
         ignore: ['.*']
       }
-    ])
+    ]),
+    // 打包分析注册
+    new BundleAnalyzerPlugin()
   ]
 })
 
